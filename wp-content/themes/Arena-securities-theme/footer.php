703197fa35a193b2f39aac0927bd7f99d5e24a07
LukeBertus <127717618+LukeBertus@users.noreply.github.com>
2024-01-29 20:11:10 +0800

    Make Footer Responsive

--- wp-content/themes/Arena-securities-theme/footer.php
+++ wp-content/themes/Arena-securities-theme/footer.php
@@ -13,9 +13,13 @@
                 The information in this document is not personal financial product advice and has been prepared without taking into account the objectives, financial situation or needs of any particular person. Arena Securities does not hold an Australian Financial Services License and is not authorised to provide general or personal financial product advice. Before making any investment decision, you should consult a licensed financial adviser. Past performance is not indicative of future performance.
                 </p>
             </div>
-            <div class="footer__lower">
+            <div class="footer__lower footer__lower--desktop">
                 <img src="<?=get_theme_file_uri('/images/logo.png')?>" alt="logo">
                 <p class="copyright">Copyright &copy; 2024 Arena Securities | Design by WallabyWeb</p>
+            </div>
+            <div class="footer__lower footer__lower--mobile">
+                <p class="copyright">Copyright &copy; 2024 Arena Securities | Design by WallabyWeb</p>
+            </div>
         </div>
     </div>
 </body>
