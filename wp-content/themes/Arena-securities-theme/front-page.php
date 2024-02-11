eb03d4f1e633858c7f0aa9b0a2353eba7f70bf08
LukeBertus <127717618+LukeBertus@users.noreply.github.com>
2024-02-11 11:55:23 +0800

    Add Hero Skeleton

--- wp-content/themes/Arena-securities-theme/front-page.php
+++ wp-content/themes/Arena-securities-theme/front-page.php
@@ -2,8 +2,14 @@
     get_header();
 ?>
 
-<div class="hero section__outer" style="background-image: url('<?=get_theme_file_uri('images/hero.jpg')?>'); background-size: cover;">
-    <div class="section__inner"></div>
+<div class="hero section__outer" style="background-image: url('<?=get_theme_file_uri('images/hero.jpg')?>');">
+    <div class="section__inner">
+        <div class="title_container">
+            <h1 class="page_title">ARENA SECURITIES</h1>
+            <hr>
+            <p class="page_subtitle">A Private Investment Company</p>
+        </div>
+    </div>
 </div>
 
 <?php 
