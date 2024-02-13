e475a1eb819e7282785b9bdd4a126a6c1e0bf6de
LukeBertus <127717618+LukeBertus@users.noreply.github.com>
2024-02-13 20:36:05 +0800

    Add Header Shadow

--- wp-content/themes/Arena-securities-theme/header.php
+++ wp-content/themes/Arena-securities-theme/header.php
@@ -6,22 +6,23 @@
     <?php wp_head(); ?>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="#">
-            <img src="<?=get_theme_file_uri('/images/logo.png')?>" width="150" alt="site logo">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            
-            <div class="navbar-nav">
-                <a class="nav-item nav-link" href="#">HOME</a>
-                <a class="nav-item nav-link" href="#">MEDIA</a>
-                <a class="nav-item nav-link" href="#">BLOG</a>
-                <a class="nav-item nav-link" href="#">UPDATES</a>
-                <a class="nav-item nav-link" href="#">ABOUT</a>
+    <div class="header-container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#">
+                <img src="<?=get_theme_file_uri('/images/logo.png')?>" width="150" alt="site logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="#">HOME</a>
+                    <a class="nav-item nav-link" href="#">MEDIA</a>
+                    <a class="nav-item nav-link" href="#">BLOG</a>
+                    <a class="nav-item nav-link" href="#">UPDATES</a>
+                    <a class="nav-item nav-link" href="#">ABOUT</a>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
 </body>
