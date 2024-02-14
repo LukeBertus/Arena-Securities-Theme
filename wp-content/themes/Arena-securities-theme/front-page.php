78451f8e36b1d5e4441f30e7ab21a2c42ca18fde
LukeBertus <127717618+LukeBertus@users.noreply.github.com>
2024-02-14 19:25:11 +0800

    Add Info Section HTML

--- wp-content/themes/Arena-securities-theme/front-page.php
+++ wp-content/themes/Arena-securities-theme/front-page.php
@@ -14,6 +14,19 @@
     </div>
 </div>
 
+<div class="company-info section__outer">
+    <div class="section__inner">
+        <div class="info-left">
+            <h1>OUR COMPANY</h1>
+            <p>Arena Securities is a boutique funds management firm based in Perth, Western Australia. Founded by CIO Aleck Arena, the firm operates with a focus on absolute performance, driven by a commitment to deep fundamental research and understanding of the businesses that we invest in. Arena Securities utilises a go anywhere approach as long as the manager deems the security to be in his circle of competence. Arena Securities is a high-conviction fund and holds between 6-30 positions.</p>
+            <button>CONTACT US</button>
+        </div>
+        <div class="info-right">
+            <img src="<?=get_theme_file_uri('images/tree.png')?>">
+        </div>
+    </div>
+</div>
+
 <?php 
     get_footer();
 ?>
