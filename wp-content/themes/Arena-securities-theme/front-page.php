bfca476cc526a7dc2e85639547e7a9d50c428216
LukeBertus <127717618+LukeBertus@users.noreply.github.com>
2024-02-15 20:22:10 +0800

    Implement Info Section Sass

--- wp-content/themes/Arena-securities-theme/front-page.php
+++ wp-content/themes/Arena-securities-theme/front-page.php
@@ -16,12 +16,17 @@
 
 <div class="company-info section__outer">
     <div class="section__inner">
-        <div class="info-left">
-            <h1>OUR COMPANY</h1>
-            <p>Arena Securities is a boutique funds management firm based in Perth, Western Australia. Founded by CIO Aleck Arena, the firm operates with a focus on absolute performance, driven by a commitment to deep fundamental research and understanding of the businesses that we invest in. Arena Securities utilises a go anywhere approach as long as the manager deems the security to be in his circle of competence. Arena Securities is a high-conviction fund and holds between 6-30 positions.</p>
+        <div class="info info__left">
+            <div class="info-title">
+                <svg width="3" height="40">
+                    <rect width="3" height="40" rx="2" ry="2" stroke-width="1"/>
+                </svg>
+                <h1>OUR COMPANY</h1>
+            </div>
+            <p>Arena Securities is a boutique funds management firm based in Perth, Western Australia. Founded by CIO Aleck Arena, the firm operates with a focus on absolute performance, driven by a commitment to deep fundamental research and understanding of the businesses that we invest in.<br><br> Arena Securities utilises a go anywhere approach as long as the manager deems the security to be in his circle of competence. Arena Securities is a high-conviction fund and holds between 6-30 positions.</p>
             <button>CONTACT US</button>
         </div>
-        <div class="info-right">
+        <div class="info info__right">
             <img src="<?=get_theme_file_uri('images/tree.png')?>">
         </div>
     </div>
