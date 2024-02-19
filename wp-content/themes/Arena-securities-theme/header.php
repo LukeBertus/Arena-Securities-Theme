763a57a7b312ec38429b795169a6fb8fd7927aa4
LukeBertus <127717618+LukeBertus@users.noreply.github.com>
2024-02-19 16:24:45 +0800

    Add Header 'Home' Logic Use php to check if current page is the home page and if so highlight link green

--- wp-content/themes/Arena-securities-theme/header.php
+++ wp-content/themes/Arena-securities-theme/header.php
@@ -16,7 +16,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-item nav-link" href="#">HOME</a>
+                    <a class="nav-item nav-link <?php if (is_front_page()) echo "nav-active"?>" href="<?=esc_url(home_url())?>">HOME</a>
                     <a class="nav-item nav-link" href="#">MEDIA</a>
                     <a class="nav-item nav-link" href="#">BLOG</a>
                     <a class="nav-item nav-link" href="#">UPDATES</a>
