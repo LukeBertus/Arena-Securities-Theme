98ae764fba89b76ec78b6478275cdae01fbf6a69
LukeBertus <127717618+LukeBertus@users.noreply.github.com>
2024-02-22 20:07:56 +0800

    Add Archive Skeleton HTML

---
+++ wp-content/themes/Arena-securities-theme/archive-blog.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        get_header();
+    ?>
+    <span class="archive">
+        <div class="media-hero section__outer">
+            <div class="seciton__inner">
+                <h1>Blog</h1>
+            </div>
+        </div>
+        <div class="media-body section__outer">
+            <div class="section__inner">
+            
+            </div>
+        </div>
+    </span>
+    <?php
+        get_footer();
+    ?>
+</body>
+</html>
