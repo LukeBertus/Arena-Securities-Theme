86edca9cc2f07dd940bafd73fffb7e43fd217c61
LukeBertus <127717618+LukeBertus@users.noreply.github.com>
2024-03-02 15:17:46 +0800

    Implement PHP Logic To Pull Media Posts to Media Archive

--- wp-content/themes/Arena-securities-theme/archive-files.php
+++ wp-content/themes/Arena-securities-theme/archive-files.php
@@ -17,42 +17,7 @@
         </div>
         <div class="archive-body media-body section__outer">
             <div class="section__inner">
-                <div class="archive-container">
-                    <a href="#"><h1>Media Title</h1></a>
-                    <p class="excerpt">This is some dummy text. Just seeing how it looks</p>
-                    <div class="link"><a href="#">READ MORE</a><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M507-480 384-357l56 57 180-180-180-180-56 57 123 123ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></a></div>
-                    <hr>
-                    <p class="date">September 1, 2023</p>
-                </div>
-                <div class="archive-container">
-                    <a href="#"><h1>Media Title, This One is Longer</h1></a>
-                    <p class="excerpt">This is some dummy text. Just seeing how it looks</p>
-                    <div class="link"><a href="#">READ MORE</a><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M507-480 384-357l56 57 180-180-180-180-56 57 123 123ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></a></div>
-                    <hr>
-                    <p class="date">September 1, 2023</p>
-                </div>
-                <div class="archive-container">
-                    <a href="#"><h1>Media Title</h1></a>
-                    <p class="excerpt">This is some dummy text. Just seeing how it looks. this card has even more content, increasing its size drastically.</p>
-                    <div class="link"><a href="#">READ MORE</a><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M507-480 384-357l56 57 180-180-180-180-56 57 123 123ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></a></div>
-                    <hr>
-                    <p class="date">September 1, 2023</p>
-                </div>
-                <div class="archive-container">
-                    <a href="#"><h1>Media Title</h1></a>
-                    <p class="excerpt">This is some dummy text. Just seeing how it looks</p>
-                    <div class="link"><a href="#">READ MORE</a><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M507-480 384-357l56 57 180-180-180-180-56 57 123 123ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></a></div>
-                    <hr>
-                    <p class="date">September 1, 2023</p>
-                </div>
-                
             <?php
-                $ValuePosts = new WP_Query([
-                    'posts_per_page' => 4,
-                    'post_type' => 'values',
-                    'order' => 'ASC'
-                ]);
-
                 if (have_posts()) {
                     while(have_posts()) {
                         the_post();
